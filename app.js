fd530bb5c971a36986567e7419d4bb22878d5c4a
olivia pasion <oap9820@gmail.com>
2022-06-01 16:31:30 -0700

    TOS checkbox enables button

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // grab DOM elements
+const tosCheckbox = document.getElementById('tos-checkbox');
+const showAnswerButton = document.getElementById('show-answer-button')
+const catSection = document.getElementById('cat-section');
 
+const nameInput = document.getElementById('name-input');
+const nameDisplay = document.getElementById('name-display');
+const imageSelect = document.getElementById('image-select');
+const imageDisplay = document.getElementById('image-display');
 
 
 // set event listeners
+
+tosCheckbox.addEventListener('change', () => {
+    showAnswerButton.disabled = !tosCheckbox.checked;
+});
+
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
